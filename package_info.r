6c31aee2ae2aa122759ae3e820dc5450f4ce78c9
fischeruu <tobias.fischer@uni-ulm.de>
2016-05-10 23:27:06 +0200

    Delete package_info.r

--- package_info.r
+++
@@ -1,9 +0,0 @@
-ps.pkg.info = function() {
-  # Name of this package
-  package = "RTutorNAFTAfreetrade"
-  
-  # Name of problem sets in the package
-  ps = c("NAFTA")
-  
-  list(package=package, ps = ps)
-}
